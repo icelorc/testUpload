81a97cd3ba9aa30420322d5b15d1a938b65f1462
icelorc <icelorc@gmail.com>
2014-04-17 16:45:21 +0800

    init

--- testUpload/fontechFirstViewController.h
+++ testUpload/fontechFirstViewController.h
@@ -10,5 +10,6 @@
 #import "CommunicateWithInternet.h"
 
 @interface fontechFirstViewController : UIViewController
+- (void) upload;
 
 @end
--- testUpload/fontechFirstViewController.m
+++ testUpload/fontechFirstViewController.m
@@ -19,13 +19,91 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    NSData *data = [CommunicateWithInternet getJsonDataFromUrl:@"http://testjson.herokuapp.com/posts.json?auth_token=-xeG-o8xcyTJd9bz-Gwf" WithPostParams:[NSDictionary dictionaryWithObjectsAndKeys:@"name",@"hello",@"description", @"MynameisRobert",nil]];
+    /*
+    NSData *data = [CommunicateWithInternet getJsonDataFromUrl:@"http://testjson.herokuapp.com/products.json" WithPostParams:[NSDictionary dictionaryWithObjectsAndKeys:@"name",@"hello",@"description", @"MynameisRobert",nil]];
     
     
     
     id json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
     
     NSLog(@"%@", json);
+    */
+    
+    [self upload];
+    
+    
+    
+    
+}
+
+- (void) upload {
+    
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setCachePolicy:NSURLRequestReloadIgnoringLocalCacheData];
+    [request setHTTPShouldHandleCookies:NO];
+    [request setTimeoutInterval:30];
+    [request setHTTPMethod:@"POST"];
+    // RP: Empaquetando datos
+    NSMutableDictionary* _params = [[NSMutableDictionary alloc] init];
+    [_params setObject:@"Robert"forKey:@"name"];
+    [_params setObject:@"Founder" forKey:@"description"];
+    
+    // the boundary string : a random string, that will not repeat in post data, to separate post data fields.
+    NSString *BoundaryConstant = @"V2ymHFg03ehbqgZCaKO6jy";
+    
+    // string constant for the post parameter 'file'
+    NSString *FileParamConstant = @"image_field";
+    
+    //RP: Configurando la dirección
+    NSURL *requestURL = [[NSURL alloc] initWithString:@"http://testjson.herokuapp.com/products"];
+    
+    // set Content-Type in HTTP header
+    NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@", BoundaryConstant];
+    [request setValue:contentType forHTTPHeaderField: @"Content-Type"];
+    
+    // post body
+    NSMutableData *body = [NSMutableData data];
+    
+    // add params (all params are strings)
+    for (NSString *param in _params) {
+        [body appendData:[[NSString stringWithFormat:@"--%@\r\n", BoundaryConstant] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n", param] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"%@\r\n", [_params objectForKey:param]] dataUsingEncoding:NSUTF8StringEncoding]];
+    }
+    
+
+    // add image data
+    UIImage *image = [UIImage imageNamed:@"01.jpg"];
+    NSData *imageData = UIImageJPEGRepresentation(image, 1.0);;
+    if (imageData) {
+        printf("appending image data\n");
+        [body appendData:[[NSString stringWithFormat:@"--%@\r\n", BoundaryConstant] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"; filename=\"image.jpg\"\r\n", FileParamConstant] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[@"Content-Type: image/jpeg\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:imageData];
+        [body appendData:[[NSString stringWithFormat:@"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+    }
+    
+    [body appendData:[[NSString stringWithFormat:@"--%@--\r\n", BoundaryConstant] dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    
+    // setting the body of the post to the reqeust
+    [request setHTTPBody:body];
+    
+    // set the content-length
+    // set the content-length
+    NSString *postLength = [NSString stringWithFormat:@"%d", [body length]];
+    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+    
+    // set URL
+    [request setURL:requestURL];
+    
+    NSURLResponse *response = nil;
+    NSError *err = nil;
+    NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&err];
+    NSString *str = [[NSString alloc] initWithBytes:[data bytes] length:[data length] encoding:NSUTF8StringEncoding];
+    NSLog(@"Response : %@",str);
+    
 }
 
 - (void)didReceiveMemoryWarning
